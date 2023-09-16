fd39f7a91ac133cbd9bb6bd453d327b3734b73b9
Shashank Devadiga <beast.steam002@gmail.com>
2023-09-17 00:21:27 +0530

    Updated Files

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+import openai
+
+app = Flask(__name__)
+
+# Set up OpenAI API credentials
+openai.api_key = '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'
+
+# Define the default route to return the index.html file
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+# Define the /api route to handle POST requests
+@app.route("/api", methods=["POST"])
+def api():
+    # Get the message from the POST request
+    user_message = request.json.get("message")
+    
+    # Define a system message to set context as medical or non-medical
+    system_message = {"role": "system", "content": "You are in a medical context."}
+    
+    # Send the system message and user message to OpenAI's API and receive the response
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            system_message,
+            {"role": "user", "content": user_message}
+        ]
+    )
+    
+    response = completion.choices[0].message
+    return response
+
+if __name__ == '__main__':
+    app.run()
